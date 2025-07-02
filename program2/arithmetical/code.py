e166612ac71769b52e9c68850ba6346415d27243
cahyorom <gantengcahyo27@gmail.com>
2025-07-02 16:02:34 +0700

    Initial Commit

---
+++ program2/arithmetical/code.py
@@ -0,0 +1,11 @@
+# Addition
+num1 = float(input("Enter the first number of addition:  "))
+num2 = float(input("Enter the second number of addition:  "))
+sum_result = num1 + num2
+print(f"sum: {num1} + {num2} = {sum_result} ")
+
+
+# num1 adalah nama variabel yang telah di deklarasikan
+# float berguna sebagai mengubah tipe data string menjadi number dan float juga masuk dalam kategori built-in function
+# input juga sama built-in dan fungsinya adalah memungkinkan user untuk memasukkan nilai pada console
+# print seperti namanya adalah menampilkan hasil kedalam console
