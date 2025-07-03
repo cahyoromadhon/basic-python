53033f68cfba07726b9b31a66b36280ed7bdbeb9
cahyorom <gantengcahyo27@gmail.com>
2025-07-03 15:12:04 +0700

    Initial Commit

---
+++ program3/triangle/code.py
@@ -0,0 +1,15 @@
+# Input Value
+alas = float(input("Masukkan Nilai Alas:  "))
+tinggi = float(input("Masukkan Nilai Tinggi:  "))
+
+# Calculate the area
+area = 0.5 * alas * tinggi
+
+# Display
+print(f"Luas Segitiganya adalah: {area}")
+
+# Base dan Height adalah variabel yang telah dideklarasikan
+# Float adalah built-in function yang dapat mengubah tipe data string menjadi number
+# f"" atau yang biasa disebut f-string memungkinkan developer dapat memasukkan variabel kedalam string
+# f-string merupakan versi ringkas dari .format(parameter) dan kali ini tidak akan dibahas
+# print berguna menampilkan string kedalam console
