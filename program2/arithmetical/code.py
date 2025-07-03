a32f8d98f34e03c23a246b63c5935156ecc4cd2b
cahyorom <gantengcahyo27@gmail.com>
2025-07-03 23:19:59 +0700

    Division Arithmetical

--- program2/arithmetical/code.py
+++ program2/arithmetical/code.py
@@ -9,3 +9,12 @@ print(f"sum: {num1} + {num2} = {sum_result} ")
 # float berguna sebagai mengubah tipe data string menjadi number dan float juga masuk dalam kategori built-in function
 # input juga sama built-in dan fungsinya adalah memungkinkan user untuk memasukkan nilai pada console
 # print seperti namanya adalah menampilkan hasil kedalam console
+
+# Division
+num3 = float(input("Masukkan Nomor Pembagian:  "))
+num4 = float(input("Masukkan Nomor Pembagian:  "))
+if num4 == 0:
+    print("Error: Pembagian 0 tidak diperbolehkan")
+else:
+    divre = num3 / num4
+    print(f"Hasil Pembagian: {num3} / {num4} = {divre}")
