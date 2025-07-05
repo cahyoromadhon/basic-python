202901144d77f502ad532b58d0bd89169e1cb807
cahyorom <gantengcahyo27@gmail.com>
2025-07-05 23:26:49 +0700

    Adding Meters Conversion

--- program6/code.py
+++ program6/code.py
@@ -12,3 +12,10 @@ print(f"{kilometers} KM sama dengan {miles} Miles")
 # diikuti dengan deklarasi konversi terlebih dahulu
 # kemudian tinggal dikalikan saja kilometer dengan konversi nya yang akan menghasilkan miles
 # print menggunakan f-string agar variabel dapat dimasukkan kedalam string
+
+# sekarang aku ingin mencoba untuk mengonversikan km to m
+# jika 1 KM adalah 1.000 M maka aku hanya perlu membaginya 1.000
+inputKM = float(input("Masukkan Angka (KM):  "))
+
+meter = inputKM * 1000
+print(f"{inputKM} KM sama dengan {meter} M")
