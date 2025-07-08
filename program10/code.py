43179f1138dea0301b5106515ec34f048dbbfe2d
cahyorom <gantengcahyo27@gmail.com>
2025-07-08 23:59:54 +0700

    Program Cek Ganjil Genap

--- program10/code.py
+++ program10/code.py
@@ -8,3 +8,15 @@ else:
     print(f"{inp} adalah bilangan Ganjil")
 
 # program yang sangat mudah, ini hanyalah permainan if else yang dimana jika integer yang dimasukkan user menggunakan input bisa dibagi 2 itu artinya bilangan tersebut adalah genap dan jika tidak atau else maka bilangan tersebut adalah ganjil. that's it
+
+# aku ingin membuat program sejenis namun kali ini nilai yang dimasukkan haruslah angka apabila bukan maka akan muncul teks yang menunjukkan itu salah
+
+try:
+    inp1 = int(input("Masukkan Angka:  "))
+
+    if inp1%2 == 0:
+        print(f"{inp1} adalah bilangan Genap")
+    elif inp1%2 == 1:
+        print(f"{inp1} adalah bilangan Ganjil")
+except ValueError:
+    print("(Maaf, Hanya Menerima Angka)")
