13e4f938af0a3b19ce2baa86652618ddddeb7285
cahyorom <gantengcahyo27@gmail.com>
2025-07-08 23:48:39 +0700

    Mini Program Cek Bilangan

---
+++ program10/code.py
@@ -0,0 +1,10 @@
+# program ini akan mengecek apakah angka yang diberikan genap atau ganjil
+
+inp = int(input("Masukkan Angka:  "))
+
+if inp%2 == 0:
+    print(f"{inp} adalah bilangan Genap")
+else:
+    print(f"{inp} adalah bilangan Ganjil")
+
+# program yang sangat mudah, ini hanyalah permainan if else yang dimana jika integer yang dimasukkan user menggunakan input bisa dibagi 2 itu artinya bilangan tersebut adalah genap dan jika tidak atau else maka bilangan tersebut adalah ganjil. that's it
